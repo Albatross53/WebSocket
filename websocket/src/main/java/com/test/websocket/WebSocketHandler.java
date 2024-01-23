a5399e2eb0c62907e43b37a608c927ad363e8062
jeon hyebeen <hyebin199953@gmail.com>
2024-01-23 21:18:24 +0900

    feat/ webSocket handler

--- websocket/src/main/java/com/test/websocket/WebSocketHandler.java
+++ websocket/src/main/java/com/test/websocket/WebSocketHandler.java
@@ -1,4 +1,17 @@
 package com.test.websocket;
 
-public class WebSocketHandler {
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String receivedMessage = message.getPayload();
+        System.out.println("Received message: " + receivedMessage);
+        session.sendMessage(new TextMessage("Server received your message: " + receivedMessage));
+    }
 }
